a694ece2d33d7029bb322ece5a334520f4e83c04
azafty468 <azafty@gmail.com>
2012-10-01 14:41:13 -0400

    accelerators for BMP drawing

--- src/gameObjects/Board.java
+++ src/gameObjects/Board.java
@@ -1,5 +1,8 @@
 package gameObjects;
 
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -34,10 +37,10 @@ public class Board {
 		GameObjectBackground boundaryOpen = new GameObjectBackground();
 		try {
 			BufferedImage imgBlocked = ImageIO.read(new File("images\\TestObstruction.bmp"));
-			boundaryWall.setGraphics(imgBlocked);
+			boundaryWall.setGraphics(convertImageToLocalSettings(imgBlocked));
 			
 			BufferedImage imgOpen = ImageIO.read(new File("images\\BackgroundEmpty.bmp"));
-			boundaryOpen.setGraphics(imgOpen);
+			boundaryOpen.setGraphics(convertImageToLocalSettings(imgOpen));
 		}
 		catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Error while loading base graphics with exception: " + e.getMessage());
@@ -54,5 +57,26 @@ public class Board {
 					
 			}
 		}
+	}
+
+	private BufferedImage convertImageToLocalSettings(BufferedImage input) {
+		GraphicsConfiguration gfx_config = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration(); 
+
+		// if image is already compatible and optimized for current system settings, simply return it 
+		if (input.getColorModel().equals(gfx_config.getColorModel())) 
+			return input; 
+		
+		// image is not optimized, so create a new image that is 
+		BufferedImage new_image = gfx_config.createCompatibleImage(input.getWidth(), input.getHeight(), input.getTransparency()); 
+
+		// get the graphics context of the new image to draw the old image on 
+		Graphics2D g2d = (Graphics2D) new_image.getGraphics(); 
+
+		// actually draw the image and dispose of context no longer needed 
+		g2d.drawImage(input, 0, 0, null); 
+		g2d.dispose(); 
+
+		// return the new optimized image 
+		return new_image; 
 	}
 }
